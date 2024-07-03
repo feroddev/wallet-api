a4b0eaf529a800626f0a2d8ddf71e2f21cbd8520
Felipe Rodrigues <neath.felipe@gmail.com>
2024-07-03 14:40:20 -0300

    feat: Add user entity, dto, service, and module

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} user`;
+  }
+}
