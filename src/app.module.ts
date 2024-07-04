ebcdbe2d6b4c7bbd82fd232a0cb04d742dd1ad90
Felipe Rodrigues <neath.felipe@gmail.com>
2024-07-03 22:58:16 -0300

    chore: Remove unused user-related files and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { SignInModule } from './sign-in/sign-in.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, SignInModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
