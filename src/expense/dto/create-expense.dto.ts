bf8cadc2b457ed7065657ad1b5725e64bb5d19d3
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-08-18 13:35:36 -0300

    feat: Update CreateExpenseDto to require description field

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,22 +1,30 @@
-import { Transform, Type } from "class-transformer";
-import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
+import { Transform, Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
 
 export class CreateExpenseDto {
   @IsNumber()
   @IsPositive()
   @Transform(({ value }) => Number(value))
   amount: number;
-  
+
   @IsOptional()
   @IsString()
   @Transform(({ value }) => String(value))
-  description?: string;
-  
+  description: string;
+
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) => String(value))
   categoryId: string;
-  
+
   @IsNotEmpty()
   @IsDate()
   @Type(() => Date)
