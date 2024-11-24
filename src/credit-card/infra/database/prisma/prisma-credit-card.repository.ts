1abd38064028c3e5df8e0156c5c39b7438505ea3
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-11-24 01:04:09 -0300

    refactor: simplificar a formatação e melhorar a legibilidade dos repositórios Prisma de cartões de crédito

--- src/credit-card/infra/database/prisma/prisma-credit-card.repository.ts
+++ src/credit-card/infra/database/prisma/prisma-credit-card.repository.ts
@@ -1,38 +1,40 @@
+import { CreditCard } from '@prisma/client'
+import { PrismaService } from '../../../../prisma/prisma.service'
+import { CreditCardRepository } from '../../../repositories/credit-card.repository'
+
 export class PrismaCreditCardRepository implements CreditCardRepository {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(data: CreditCard): Promise<CreditCard> {
-      return this.prisma.creditCard.create({
-        data,
-      })
+    return this.prisma.creditCard.create({
+      data
+    })
   }
 
   async find(data: Partial<CreditCard>): Promise<CreditCard> {
-      return this.prisma.creditCard.findFirst({
-        where: data,
-      })
+    return this.prisma.creditCard.findFirst({
+      where: data
+    })
   }
 
   async findMany(data: Partial<CreditCard>): Promise<CreditCard[]> {
-      return this.prisma.creditCard.findMany({
-        where: data,
-      })
+    return this.prisma.creditCard.findMany({
+      where: data
+    })
   }
 
   async update(id: string, data: Partial<CreditCard>): Promise<CreditCard> {
-      return this.prisma.creditCard.findMany({
-        where: data,
-      })
+    return this.prisma.creditCard.update({
+      where: { id },
+      data
+    })
   }
 
   async delete(id: string) {
-    await this.prisma.creditCard.update({
+    await this.prisma.creditCard.delete({
       where: {
-        id,
-      },
-      data: {
-        deletedAt: new Date(),
-      },
+        id
+      }
     })
   }
 }
--- src/credit-card/repositories/credit-card.repository.ts
+++ src/credit-card/repositories/credit-card.repository.ts
@@ -1,11 +1,13 @@
+import { CreditCard } from '@prisma/client'
+
 export abstract class CreditCardRepository {
-    abstract create(data: CreditCard): Promise<CreditCard>
+  abstract create(data: CreditCard): Promise<CreditCard>
 
-    abstract find(data: Partial<CreditCard>): Promise<CreditCard>
+  abstract find(data: Partial<CreditCard>): Promise<CreditCard>
 
-    abstract findMany(data: Partial<CreditCard>): Promise<CreditCard[]>
+  abstract findMany(data: Partial<CreditCard>): Promise<CreditCard[]>
 
-    abstract update(id: string, data: Partial<CreditCard>): Promise<CreditCard>
+  abstract update(id: string, data: Partial<CreditCard>): Promise<CreditCard>
 
-    abstract delete(id: string): Promise<void>
+  abstract delete(id: string): Promise<void>
 }
