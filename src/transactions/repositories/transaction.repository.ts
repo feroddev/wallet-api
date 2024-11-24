e7e7ebeddf0cec04d8e7d4a62c659afb2e515a5f
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-11-24 01:03:14 -0300

    refactor: ajustar a formatação do repositório de transações para melhorar a legibilidade

--- src/transactions/repositories/transaction.repository.ts
+++ src/transactions/repositories/transaction.repository.ts
@@ -1,11 +1,13 @@
+import { Transaction } from '@prisma/client'
+
 export abstract class TransactionRepository {
-    abstract create(data: Transaction): Promise<Transaction>
+  abstract create(data: Transaction): Promise<Transaction>
 
-    abstract find(data: Partial<Transaction>): Promise<Transaction>
+  abstract find(data: Partial<Transaction>): Promise<Transaction>
 
-    abstract findMany(data: Partial<Transaction>): Promise<Transaction[]>
+  abstract findMany(data: Partial<Transaction>): Promise<Transaction[]>
 
-    abstract update(id: string, data: Partial<Transaction>): Promise<Transaction>
+  abstract update(id: string, data: Partial<Transaction>): Promise<Transaction>
 
-    abstract delete(id: string): Promise<void>
+  abstract delete(id: string): Promise<void>
 }
