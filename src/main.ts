bb1e4ab67e442df08f561ac2e6953bbfa53ca41b
feroddev <neath.felipe@gmail.com>
2025-06-11 00:12:32 -0300

    feat: adiciona documentação swagger e implementa transações recorrentes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './utils/http-exception.filter'
 
@@ -20,6 +21,17 @@ async function bootstrap() {
     origin: ['http://localhost:3000', 'https://wallet-web-six.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
   })
+  
+  const config = new DocumentBuilder()
+    .setTitle('Wallet API')
+    .setDescription('API para gerenciamento de finanças pessoais')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+    
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api/docs', app, document)
+  
   await app.listen(process.env.PORT ?? 3001)
 }
 bootstrap()
