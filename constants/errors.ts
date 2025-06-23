07d6cdbc44bf45c13c9948adb6479d4f2c511c0e
feroddev <neath.felipe@gmail.com>
2025-06-23 02:05:01 -0300

    feat: traduzir mensagens de erro para português e adicionar funcionalidade de pagamento de transações

--- constants/errors.ts
+++ constants/errors.ts
@@ -1,13 +1,23 @@
 export const errors = {
-  USER_NOT_FOUND: 'User not found',
-  INVALID_PASSWORD: 'Invalid password',
-  TOKEN_NOT_FOUND: 'Token not found',
-  INVALID_TOKEN: 'Invalid token',
-  EMAIL_ALREADY_REGISTERED: 'Email already registered',
-  GENERIC_ERROR: 'Something went wrong',
-  INVALID_FROM_DATE_AFTER_TO_DATE: 'From date cannot be after to date',
-  INSTALLMENT_NOT_FOUND: 'Installment not found',
-  TRANSACTIONS_NOT_FOUND: 'Transactions not found',
-  CREDIT_CARD_NOT_FOUND: 'Credit card not found',
-  PENDING_PAYMENT_NOT_FOUND: 'Pending payment not found'
+  USER_NOT_FOUND: 'Usuário não encontrado',
+  INVALID_PASSWORD: 'Senha inválida',
+  TOKEN_NOT_FOUND: 'Token não encontrado',
+  INVALID_TOKEN: 'Token inválido',
+  EMAIL_ALREADY_REGISTERED: 'Email já registrado',
+  GENERIC_ERROR: 'Algo deu errado',
+  INVALID_FROM_DATE_AFTER_TO_DATE:
+    'Data de início não pode ser depois da data de fim',
+  INSTALLMENT_NOT_FOUND: 'Parcelamento não encontrado',
+  CREDIT_CARD_NOT_FOUND: 'Cartão de crédito não encontrado',
+  PENDING_PAYMENT_NOT_FOUND: 'Pagamento pendente não encontrado',
+  BUDGET_NOT_FOUND: 'Orçamento não encontrado',
+  GOAL_NOT_FOUND: 'Meta não encontrada',
+  CATEGORY_NOT_FOUND: 'Categoria não encontrada',
+  INVOICE_NOT_FOUND: 'Fatura não encontrada',
+  CANNOT_PAY_INVOICE_BEFORE_CLOSING_DATE:
+    'Não é possível pagar uma fatura que ainda não foi fechada',
+  TRANSACTION_NOT_FOUND: 'Transação não encontrada',
+  ID_PURCHASE_NOT_FOUND: 'ID da compra não encontrado',
+  NOT_PART_OF_A_PARCELIZED_PURCHASE:
+    'Esta transação não faz parte de uma compra parcelada'
 }
